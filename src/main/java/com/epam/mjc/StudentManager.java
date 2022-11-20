365afb2f0a81334813667f79ef52d9d13f596836
Aidar Yessembayev <yessembayevaidar@gmail.com>
2022-11-20 16:55:27 +0600

    init 2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -17,8 +17,14 @@ public class StudentManager {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+
+      try {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      } catch (IllegalArgumentException ex) {
+        System.err.print(ex);
+      }
+
     }
 
   }
